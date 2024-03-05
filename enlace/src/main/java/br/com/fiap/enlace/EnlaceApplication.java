fea63f21cc1235928928750b0fb70d1f6b9ba4b8
ninarebello <rebellonina@gmail.com>
2024-03-05 09:10:41 -0300

    adicionando projeto Spring

---
+++ enlace/src/main/java/br/com/fiap/enlace/EnlaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.enlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnlaceApplication.class, args);
+	}
+
+}
